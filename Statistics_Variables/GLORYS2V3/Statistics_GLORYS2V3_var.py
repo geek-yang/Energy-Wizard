5a0c3dd07838f1b2fd76e7cdd73d3037a28f2a11
Yang <y.liu@esciencecenter.nl>
2018-01-10 18:14:57 +0100

    Update Statistics_GLORYS2V3_var.py

--- Statistics_Variables/GLORYS2V3/Statistics_GLORYS2V3_var.py
+++ Statistics_Variables/GLORYS2V3/Statistics_GLORYS2V3_var.py
@@ -181,9 +181,9 @@ def var_coordinate(datapath):
 def field_statistics(theta_key, uv_key):
     # extract variables
     print "Start extracting variables for the quantification of meridional energy transport."
-    theta = theta_key.variables['thetao'][:] # the unit of theta is Celsius!
-    u = uv_key.variables['uo'][:]
-    v = uv_key.variables['vo'][:]
+    theta = theta_key.variables['votemper'][:] # the unit of theta is Celsius!
+    u = uv_key.variables['vozocrtx'][:]
+    v = uv_key.variables['vomecrty'][:]
     print 'Extracting variables successfully!'
     # set the filled value to be 0
     np.ma.set_fill_value(theta,0)
