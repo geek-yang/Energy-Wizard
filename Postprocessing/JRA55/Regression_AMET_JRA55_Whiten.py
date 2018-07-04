bbd46825666cc34f0b6f273575aa54161082fb66
Yang <y.liu@esciencecenter.nl>
2018-07-04 13:36:10 +0200

    Update Regression_AMET_JRA55_Whiten.py

--- Postprocessing/JRA55/Regression_AMET_JRA55_Whiten.py
+++ Postprocessing/JRA55/Regression_AMET_JRA55_Whiten.py
@@ -5,7 +5,7 @@ Copyright Netherlands eScience Center
 Function        : Regression of climatological variable on AMET (JRA55) with whitening
 Author          : Yang Liu
 Date            : 2018.01.09
-Last Update     : 2018.05.29
+Last Update     : 2018.07.04
 Description     : The code aims to explore the assotiation between climatological
                   variables with atmospheric meridional energy transport (AMET).
                   The statistical method employed here is linear regression. A
@@ -60,7 +60,7 @@ sns.set()
 # AMET
 datapath_AMET = '/home/yang/workbench/Core_Database_AMET_OMET_reanalysis/JRA55/postprocessing'
 # target climatological variables
-datapath_y = "/home/yang/workbench/Core_Database_AMET_OMET_reanalysis/ERAI/regression"
+datapath_y = "/home/yang/workbench/Core_Database_AMET_OMET_reanalysis/JRA55/regression"
 # specify output path for figures
 output_path = '/home/yang/NLeSC/Computation_Modeling/BlueAction/AMET/JRA55'
 # the threshold ( index of latitude) of the AMET
@@ -87,13 +87,13 @@ lat_interest['JRA55'] = [lat_JRA55_20,lat_JRA55_30,lat_JRA55_40,lat_JRA55_50,lat
 
 # from 90N to 20N --> 0:123
 # the range ( index of latitude) of the projection field
-lat_y = 94 # upto 20 N
+lat_y = 56 # upto 20 N
 ####################################################################################
 print '*******************************************************************'
 print '*********************** extract variables *************************'
 print '*******************************************************************'
 dataset_AMET = Dataset(datapath_AMET + os.sep + 'AMET_JRA55_model_daily_1979_2015_E_zonal_int.nc')
-dataset_y = Dataset(datapath_y + os.sep + 'surface_ERAI_monthly_regress_1979_2016.nc')
+dataset_y = Dataset(datapath_y + os.sep + 'surface_JRA55_monthly_regress_1958_2013.nc')
 for k in dataset_AMET.variables:
     print dataset_AMET.variables['%s' % (k)]
 
@@ -102,24 +102,22 @@ for l in dataset_y.variables:
 
 # extract atmospheric meridional energy transport
 # dimension (year,month,latitude)
-AMET = dataset_AMET.variables['E'][:,:,0:125]/1000 # from Tera Watt to Peta Watt
-year = dataset_AMET.variables['year'][:]
+AMET = dataset_AMET.variables['E'][:-2,:,0:125]/1000 # from Tera Watt to Peta Watt
+year = dataset_AMET.variables['year'][:-2]
 lat_AMET = dataset_AMET.variables['latitude'][0:125]
 # extract variables from 20N to 90 N
 # sea level pressure
-SLP = dataset_y.variables['msl'][:444,0:lat_y+1,:] # now to 2015
+SLP = dataset_y.variables['SLP'][21:,:,0:lat_y+1,:] # now to 2013
 # sea surface temperature
-SST = dataset_y.variables['sst'][:444,0:lat_y+1,:]
-mask_SST = np.ma.getmaskarray(SST[0,:,:])
+SST = dataset_y.variables['ST'][21:,:,0:lat_y+1,:] - 273.15
+mask_SST = dataset_y.variables['mask'][0:lat_y+1,:]
 # sea ice cover
-ci = dataset_y.variables['ci'][:444,0:lat_y+1,:]
-mask_ci = np.ma.getmaskarray(ci[0,:,:])
+ci = dataset_y.variables['SIC'][21:,:,0:lat_y+1,:]
+mask_ci = dataset_y.variables['mask'][0:lat_y+1,:]
 # longitude
 lon = dataset_y.variables['longitude'][:]
 # latitude
 lat = dataset_y.variables['latitude'][0:lat_y+1]
-# time (number of months)
-time = dataset_y.variables['time'][:444]
 
 print 'The type of SLP is', type(SLP)
 print 'The type of SST is', type(SST)
@@ -132,31 +130,25 @@ print '*******************************************************************'
 # These climitology data comes from ERA-Interim surface level
 month_ind = np.arange(12)
 # remove climatology for Sea Level Pressure
-SLP_seasonal_mean = np.zeros((12,len(lat),len(lon))) # from 20N - 90N
+SLP_seasonal_cycle = np.mean(SLP,0) # from 60N - 90N
 SLP_white = np.zeros(SLP.shape,dtype=float)
 for i in month_ind:
-    # calculate the monthly mean (seasonal cycling)
-    SLP_seasonal_mean[i,:,:] = np.mean(SLP[i::12,:,:],axis=0)
     # remove seasonal mean
-    SLP_white[i::12,:,:] = SLP[i::12,:,:] - SLP_seasonal_mean[i,:,:]
+    SLP_white[:,i,:,:] = SLP[:,i,:,:] - SLP_seasonal_cycle[i,:,:]
 
 # remove climatology for Sea Surface Temperature
-SST_seasonal_mean = np.zeros((12,len(lat),len(lon))) # from 20N - 90N
+SST_seasonal_cycle = np.mean(SST,0) # from 60N - 90N
 SST_white = np.zeros(SST.shape,dtype=float)
 for i in month_ind:
-    # calculate the monthly mean (seasonal cycling)
-    SST_seasonal_mean[i,:,:] = np.mean(SST[i::12,:,:],axis=0)
     # remove seasonal mean
-    SST_white[i::12,:,:] = SST[i::12,:,:] - SST_seasonal_mean[i,:,:]
+    SST_white[:,i,:,:] = SST[:,i,:,:] - SST_seasonal_cycle[i,:,:]
 
 # remove climatology for Sea Ice Concentration
-ci_seasonal_mean = np.zeros((12,len(lat),len(lon))) # from 20N - 90N
-ci_white = np.zeros(ci.shape)
+ci_seasonal_cycle = np.mean(ci,0) # from 60N - 90N
+ci_white = np.zeros(ci.shape,dtype=float)
 for i in month_ind:
-    # calculate the monthly mean (seasonal cycling)
-    ci_seasonal_mean[i,:,:] = np.mean(ci[i::12,:,:],axis=0)
     # remove seasonal mean
-    ci_white[i::12,:,:] = ci[i::12,:,:] - ci_seasonal_mean[i,:,:]
+    ci_white[:,i,:,:] = ci[:,i,:,:] - ci_seasonal_cycle[i,:,:]
 
 # remove the seasonal cycling of AMET at 60N
 # dimension of AMET[year,month]
@@ -176,28 +168,33 @@ for i in month_ind:
 # AMET_white_series_winter[2::3] = AMET_white_series[11::12] # December
 # AMET_white_series_winter[0::3] = AMET_white_series[0::12] # Jan
 # AMET_white_series_winter[1::3] = AMET_white_series[1::12] # Feb
-
-print '*******************************************************************'
-print '***************************  Detrend  *****************************'
-print '*******************************************************************'
-####################################################
-######      detrend - polynomial fitting      ######
-####################################################
-poly_fit = np.zeros(ci_white.shape,dtype=float)
-for i in np.arange(len(lat)):
-    for j in np.arange(len(lon)):
-        polynomial = np.polyfit(np.arange(len(time)), ci_white[:,i,j], 3)
-        poly = np.poly1d(polynomial)
-        poly_fit[:,i,j] = poly(np.arange(len(time)))
-
-ci_white_detrend_poly = np.zeros(ci_white.shape,dtype=float)
-ci_white_detrend_poly = ci_white - poly_fit
 print '*******************************************************************'
 print '*********************** prepare variables *************************'
 print '*******************************************************************'
 # take the time series of E
 AMET_series = AMET.reshape(len(year)*len(month_ind),len(lat_AMET))
 AMET_white_series = AMET_white.reshape(len(year)*len(month_ind),len(lat_AMET))
+SLP_white_series = SLP_white.reshape(len(year)*len(month_ind),len(lat),len(lon))
+SST_white_series = SST_white.reshape(len(year)*len(month_ind),len(lat),len(lon))
+#ci_series = ci.reshape(len(year)*len(month_ind),len(lat),len(lon))
+ci_white_series = ci_white.reshape(len(year)*len(month_ind),len(lat),len(lon))
+print '*******************************************************************'
+print '***************************  Detrend  *****************************'
+print '*******************************************************************'
+time = np.arange(len(year)*12)
+####################################################
+######      detrend - polynomial fitting      ######
+####################################################
+poly_fit = np.zeros(ci_white_series.shape,dtype=float)
+for i in np.arange(len(lat)):
+    for j in np.arange(len(lon)):
+        polynomial = np.polyfit(np.arange(len(time)), ci_white_series[:,i,j], 3)
+        poly = np.poly1d(polynomial)
+        poly_fit[:,i,j] = poly(np.arange(len(time)))
+
+ci_white_detrend_poly = np.zeros(ci_white_series.shape,dtype=float)
+ci_white_detrend_poly = ci_white_series - poly_fit
+
 print '*******************************************************************'
 print '********************** Running mean/sum ***************************'
 print '*******************************************************************'
@@ -219,15 +216,15 @@ for i in np.arange(len(year)*len(month_ind)-window+1):
 
 SLP_white_running_mean = np.zeros((len(year)*len(month_ind)-window+1,len(lat),len(lon)),dtype=float)
 for i in np.arange(len(year)*len(month_ind)-window+1):
-    SLP_white_running_mean[i,:,:] = np.mean(SLP_white[i:i+window,:,:],0)
+    SLP_white_running_mean[i,:,:] = np.mean(SLP_white_series[i:i+window,:,:],0)
 
 SST_white_running_mean = np.zeros((len(year)*len(month_ind)-window+1,len(lat),len(lon)),dtype=float)
 for i in np.arange(len(year)*len(month_ind)-window+1):
-    SST_white_running_mean[i,:,:] = np.mean(SST_white[i:i+window,:,:],0)
+    SST_white_running_mean[i,:,:] = np.mean(SST_white_series[i:i+window,:,:],0)
 
 ci_white_running_mean = np.zeros((len(year)*len(month_ind)-window+1,len(lat),len(lon)),dtype=float)
 for i in np.arange(len(year)*len(month_ind)-window+1):
-    ci_white_running_mean[i,:,:] = np.mean(ci_white[i:i+window,:,:],0)
+    ci_white_running_mean[i,:,:] = np.mean(ci_white_series[i:i+window,:,:],0)
 
 ci_white_detrend_poly_running_mean = np.zeros((len(year)*len(month_ind)-window+1,len(lat),len(lon)),dtype=float)
 for i in np.arange(len(year)*len(month_ind)-window+1):
@@ -237,8 +234,8 @@ print '*******************************************************************'
 print '*************************** time series ***************************'
 print '*******************************************************************'
 # index and namelist of years for time series and running mean time series
-index = np.arange(1,445,1)
-index_year = np.arange(1979,2016,1)
+index = np.arange(1,421,1)
+index_year = np.arange(1979,2014,1)
 
 # #index_running_mean = np.arange(1,457-window+1,1)
 # #index_year_running_mean = np.arange(1979+window/12-1,2017,1)
@@ -372,6 +369,24 @@ index_year = np.arange(1979,2016,1)
 # plt.show()
 # fig10.savefig(output_path + os.sep + 'AMET_anomaly_60N_FFT_running_mean_%d_1979_2015.jpg' % (window), dpi = 500)
 
+# testing figure for detrending with time series excluding seasonal cycling
+# detrend - polynomial fitting
+fig002 = plt.figure()
+plt.axhline(y=0, color='k',ls='-')
+plt.plot(index,np.mean(np.mean(ci_white_series,2),1),'b--',linewidth = 0.5,label='Anomalies')
+plt.plot(index,np.mean(np.mean(poly_fit,2),1),'r-',linewidth = 2,label='Running mean anomalies')
+plt.plot(index,np.mean(np.mean(ci_white_detrend_poly,2),1),'g-',linewidth = 1,label='Detrend anomalies')
+#plt.plot(index[window_detrend-1:],np.mean(np.mean(ci_white_test,2),1),'m-',linewidth = 1,label='xxx')
+plt.title('Sea Ice Concentration Anomalies in the Arctic and the detrend SIC anomalies (1979-2013)')
+plt.legend()
+fig002.set_size_inches(12, 5)
+plt.xlabel("Time")
+plt.xticks(np.linspace(0, 420, 36), index_year)
+plt.xticks(rotation=60)
+plt.ylabel("Sea Ice Concentration (Percentage)")
+plt.show()
+fig002.savefig(output_path + os.sep + 'Detrend_poly_JRA55_ice_white.jpg', dpi = 300)
+
 print '*******************************************************************'
 print '**************************** trend ********************************'
 print '*******************************************************************'
@@ -389,7 +404,7 @@ A = np.vstack([index,np.ones(len(index))]).T
 for i in np.arange(lat_y+1):
     for j in np.arange(len(lon)):
         # return value: coefficient matrix a and b, where a is the slope
-        a[i,j], b[i,j] = np.linalg.lstsq(A,SLP_white[:,i,j])[0]
+        a[i,j], b[i,j] = np.linalg.lstsq(A,SLP_white_series[:,i,j])[0]
 # visualization through basemap
 fig11 = plt.figure()
 # setup north polar stereographic basemap
@@ -411,23 +426,23 @@ color = np.linspace(-45,45,19) # for SLP
 # !!!!!take care about the coordinate of contourf(Longitude, Latitude, data(Lat,Lon))
 #cs = m.contour(XX,YY,a*12,color)
 #plt.clabel(cs, fontsize=7, inline=1)
-cs = m.contourf(XX,YY,a*12*10,color,cmap='coolwarm')
+cs = m.contourf(XX,YY,a*12*10,color,cmap='coolwarm',extend='both')
 # add color bar
 cbar = m.colorbar(cs,location="bottom",size='4%',pad="8%",format='%d')
 cbar.ax.tick_params(labelsize=8)
 #cbar.set_ticks(np.arange(-1,1.1,0.2))
 #cbar.set_ticklabels(np.arange(-1,1.1,0.2))
 cbar.set_label('Pa/Decade',fontsize = 8)
-plt.title('Trend of Sea Level Pressure Anomalies (1979-2015)',fontsize = 9, y=1.05)
+plt.title('Trend of Sea Level Pressure Anomalies (1979-2013)',fontsize = 9, y=1.05)
 plt.show()
-fig11.savefig(output_path + os.sep + "Trend_JRA55_SLP.jpeg",dpi=500)
+fig11.savefig(output_path + os.sep + "Trend_JRA55_SLP.jpeg",dpi=400)
 
 # trend of SST
 # start the least square fitting
 for i in np.arange(lat_y+1):
     for j in np.arange(len(lon)):
         # return value: coefficient matrix a and b, where a is the slope
-        a[i,j], b[i,j] = np.linalg.lstsq(A,SST_white[:,i,j])[0]
+        a[i,j], b[i,j] = np.linalg.lstsq(A,SST_white_series[:,i,j])[0]
 # visualization through basemap
 fig12 = plt.figure()
 m = Basemap(projection='npstere',boundinglat=60,round=True,lon_0=0,resolution='l')
@@ -444,7 +459,7 @@ XX, YY = m(xx, yy)
 # define color range for the contourf
 color = np.linspace(-0.8,0.8,17)
 # !!!!!take care about the coordinate of contourf(Longitude, Latitude, data(Lat,Lon))
-cs = m.contourf(XX,YY,np.ma.masked_where(mask_SST,a*12*10),color,cmap='coolwarm')
+cs = m.contourf(XX,YY,np.ma.masked_where(mask_SST,a*12*10),color,cmap='coolwarm',extend='both')
 # add color bar
 cbar = m.colorbar(cs,location="bottom",size='4%',pad="8%",format='%.1f')
 cbar.ax.tick_params(labelsize=8)
@@ -458,7 +473,7 @@ fig12.savefig(output_path + os.sep + "Trend_JRA55_SST.jpeg",dpi=500)
 for i in np.arange(lat_y+1):
     for j in np.arange(len(lon)):
         # return value: coefficient matrix a and b, where a is the slope
-        a[i,j], b[i,j] = np.linalg.lstsq(A,ci_white[:,i,j])[0]
+        a[i,j], b[i,j] = np.linalg.lstsq(A,ci_white_series[:,i,j])[0]
 # visualization through basemap
 fig13 = plt.figure()
 m = Basemap(projection='npstere',boundinglat=60,round=True,lon_0=0,resolution='l')
@@ -475,7 +490,7 @@ XX, YY = m(xx, yy)
 # define color range for the contourf
 color = np.linspace(-0.15,0.15,21)
 # !!!!!take care about the coordinate of contourf(Longitude, Latitude, data(Lat,Lon))
-cs = m.contourf(XX,YY,np.ma.masked_where(mask_ci,a*12*10),color,cmap='coolwarm')
+cs = m.contourf(XX,YY,np.ma.masked_where(mask_ci,a*12*10),color,cmap='coolwarm',extend='both')
 # add color bar
 cbar = m.colorbar(cs,location="bottom",size='4%',pad="8%",format='%.3f')
 cbar.ax.tick_params(labelsize=8)
@@ -516,7 +531,7 @@ for c in np.arange(len(lat_interest_list)):
     for i in np.arange(lat_y+1):
         for j in np.arange(len(lon)):
             # return value: slope, intercept, r_value, p_value, stderr
-            slope[i,j],_,r_value[i,j],p_value_original[i,j],_ = stats.linregress(AMET_white_series[:,lat_interest['JRA55'][c]],SLP_white[:,i,j])
+            slope[i,j],_,r_value[i,j],p_value_original[i,j],_ = stats.linregress(AMET_white_series[:,lat_interest['JRA55'][c]],SLP_white_series[:,i,j])
     # visualization through basemap
     fig14 = plt.figure()
     # setup north polar stereographic basemap
@@ -610,14 +625,14 @@ for c in np.arange(len(lat_interest_list)):
     plt.title('Regression of SLP Anomaly on AMET Anomaly across %dN with a running mean of %d months' % (lat_interest_list[c],window),fontsize = 9, y=1.05)
     plt.show()
     fig151.savefig(output_path + os.sep + 'SLP' + os.sep + 'Interannual' + os.sep + "Regression_AMET_SLP_JRA55_white_%dN_lowpass_%dm_regression_coef.jpeg" % (lat_interest_list[c],window),dpi=400)
-    #fig151.savefig(output_path + os.sep + 'SLP' + os.sep + 'Annual' + os.sep + "Regression_AMET_SLP_JRA55_white_%dN_lowpass_%m_regression_coef.jpeg" % (lat_interest_list[c],window),dpi=400)
+    #fig151.savefig(output_path + os.sep + 'SLP' + os.sep + 'Annual' + os.sep + "Regression_AMET_SLP_JRA55_white_%dN_lowpass_%dm_regression_coef.jpeg" % (lat_interest_list[c],window),dpi=400)
 
     # linear regress SST on AMET (anomalies)
     # plot correlation coefficient
     for i in np.arange(lat_y+1):
         for j in np.arange(len(lon)):
             # return value: slope, intercept, r_value, p_value, stderr
-            slope[i,j],_,r_value[i,j],p_value[i,j],_ = stats.linregress(AMET_white_series[:,lat_interest['JRA55'][c]],SST_white[:,i,j])
+            slope[i,j],_,r_value[i,j],p_value[i,j],_ = stats.linregress(AMET_white_series[:,lat_interest['JRA55'][c]],SST_white_series[:,i,j])
     # visualization through basemap
     fig16 = plt.figure()
     # setup north polar stereographic basemap
@@ -680,7 +695,7 @@ for c in np.arange(len(lat_interest_list)):
     for i in np.arange(lat_y+1):
         for j in np.arange(len(lon)):
             # return value: slope, intercept, r_value, p_value, stderr
-            slope[i,j],_,r_value[i,j],p_value[i,j],_ = stats.linregress(AMET_white_series[:,lat_interest['JRA55'][c]],ci_white[:,i,j])
+            slope[i,j],_,r_value[i,j],p_value[i,j],_ = stats.linregress(AMET_white_series[:,lat_interest['JRA55'][c]],ci_white_series[:,i,j])
     # visualization through basemap
     fig18 = plt.figure()
     # setup north polar stereographic basemap
